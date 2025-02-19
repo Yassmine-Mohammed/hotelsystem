0996c57f35fafdf1ade7b6fc4503cb69648d2c4b
Yassmine Mohammed <yasmyn.ftha00671@compit.aun.edu.eg>
2025-02-19 14:40:28 +0200

    food and inventory systems

--- helpers final project 2025/main.cpp
+++ helpers final project 2025/main.cpp
@@ -33,59 +33,179 @@ void setcolor (int color)
     SetConsoleTextAttribute وبعد كده بتدخله في
 
     SetConsoleTextAttribute
-دي بقى دالة جاهزة في مكتبة الويندوز بتاخد برامترين الاول الحاجة اللي عايز تلونها والتاني رقم اللون اللي انتي عايزه*/
+    دي بقى دالة جاهزة في مكتبة الويندوز بتاخد برامترين الاول الحاجة اللي عايز تلونها والتاني رقم اللون اللي انتي عايزه*/
 
     /*الا بقى دالة تتشك على الان بوت انه رقم ومش حاجة تاني*/
 }
-bool isValidNumber(int &num)
+bool isValidNumber(int &num, int min, int max)
+//تاكد ان الرقم رقم وبين الاختيارات اللي عايزينها
 {
-    cin >> num;
-    if (cin.fail() || cin.peek() != '\n')
+    if (cin.fail() || cin.peek() != '\n' || num < min || num > max)
     {
-        /*cin.fail() بس بيتحقق إذا كان الإدخال كله غلط (زي لو المستخدم كتب حرف بدل رقم).
-        لكن
-          cin.peek() بتتأكد إنه ما فيش أي حاجة زيادة بعد الرقم، حتى لو كان الإدخال الأساسي صح.
-        */
         cin.clear();
         cin.ignore(10000, '\n');
         return false;
     }
     return true;
 }
+/*تعريف المتغيرات ب
+map
+بدل ما تعرف كل متغير لوحده*/
+// لو عايزة تضيفي حاجة يا ميري ضيفي!!
+map <string, int> inventory =
+{
+    {"Cheese",100}, {"Eggs",100},{"Bread",100},
+    {"Milk",100},{"Chicken",100},{"Rice",100},
+    {"Fries",100},{"Tea",100},{"Pasta",100},
+    {"Salad",100}, {"Meat",100}, {"Coffee",100},
+    {"Nuts", 100},{"Fruits",100}, {"Yogurt",100},
+    {"Juice",100}
+};
 void InventoryManagement()
 {
-    /*تعريف المتغيرات ب
-    map
-    بدل ما تعرف كل متغير لوحده*/
-    // لو عايزة تضيفي حاجة يا ميري ضيفي!!
-    map <string, int> inventory =
+    for (const auto& item : inventory)
     {
-        {"chicken", 1000},
-        {"rice", 100},
-        {"beef", 1000},
-        {"tomato", 1000},
-        {"onion", 1000},
-        {"celery", 1000},
-        {"lemon", 1000}
-    };
-
-    // عرض المخزون
-    cout << "Welcome to our inventory management system:\n\n";
-    cout << "Our remaining stock:\n";
-    for (auto item : inventory)
-    {
-        cout << item.first << " = " << item.second << endl;
+        cout << item.first << ": " << item.second << " unit(s)\n";
     }
+    cout << "======================================\n";
 }
+
+void foodandbevareg ()
+{
+    const int meals = 4;
+    string mealsName[4] = {"Breakfast Meal", "Lunch Meal", "Dinner Meal", "Snacks"};
+    string contents[4][4] =
+    {
+        {"Cheese", "Eggs", "Bread", "Milk"},
+        {"Chicken", "Rice", "Fries", "Tea"},
+        {"Pasta", "Salad", "Meat", "Coffee"},
+        {"Nuts", "Fruits", "Yogurt", "Juice"}
+    };
+    int prices[4] = {55, 210, 250, 85};
+
+    setcolor(11);
+    typetext("\t\t Welcome to Food and Beverage Management System \n");
+
+    typetext("\t\t=============<<   Hotel Menu   >>============\n");
+    setcolor(9);
+    for (int i = 0; i < meals; i++)
+    {
+        cout << i + 1 << ". " << mealsName[i] << "\tPrice = " << prices[i] << " LE\n\n";
+        this_thread::sleep_for(chrono::milliseconds(50));
+        //عشان يبقى لاين باي لاين زي ما ميري عايزة
+
+        cout << "  Contents:\n";
+        this_thread::sleep_for(chrono::milliseconds(50));
+        for (int j = 0; j < 4; j++)
+        {
+            cout << "  " << contents[i][j] << endl;
+            this_thread::sleep_for(chrono::milliseconds(50));
+        }
+        cout << "\n=============================\n";
+        this_thread::sleep_for(chrono::milliseconds(50));
+    }
+
+    int choice, quantity, price = 0;
+    string mealName;
+
+    setcolor(7);
+    cout << "Select the number of the meal you want to order (1-4): ";
+    cin >> choice;
+    //input chek
+    while (!isValidNumber(choice, 1, 4))//يبقى رقم بين ال1وال4
+    {
+        setcolor(4);
+        cout << "\n This choice isn't available. Please enter another choice (1-4): ";
+        setcolor(7);
+        cin >> choice;
+    }
+
+    switch (choice)
+        //سويتش عشان اعرف اعمل كاونتر للمخزون
+    {
+    case 1:
+        mealName = "Breakfast Meal";
+        price = prices[0];
+        break;
+    case 2:
+        mealName = "Lunch Meal";
+        price = prices[1];
+        break;
+    case 3:
+        mealName = "Dinner Meal";
+        price = prices[2];
+        break;
+    case 4:
+        mealName = "Snacks";
+        price = prices[3];
+        break;
+    }
+
+    cout << "Enter the quantity: ";
+    cin >> quantity;
+    while (!isValidNumber(quantity, 1, 100))
+    {
+        setcolor(4);
+        cout << "\n Please enter a valid quantity (1-100): ";
+        setcolor(7);
+        cin >> quantity;
+    }
+
+    int totalCost = price * quantity;
+    //الفاتورة
+
+    cout << "\n=========== Bill ===========\n";
+    cout << "Meal: " << mealName << endl;
+    cout << "Quantity: " << quantity << endl;
+    cout << "Total Cost: " << totalCost << " LE" << endl;
+    cout << "======================================\n";
+    //نستأذنك يا بشمهندسة هناخد كام حاجة من عندك
+    for( int j=0; j<4; j++)
+    {
+        // نعمل سترينج في المكونات باسم الله
+        string ingredient = contents[choice-1][j];
+        //هنا احنا بنعمل سترينج بيخزن فيها مكونات الوجبة اللي احنا عايزينها عشان اولا
+        //نتأكدان مكوناتها موجودة في اراي المخزن
+        if( inventory [ingredient] >= quantity)
+        {
+            //لو موجودة هنطرحها منه
+            //واستخدمنا لوب عشان نتأكد من انه بيطرح من كل العناصر
+            inventory[ingredient] -= quantity;
+        }
+
+        else
+        {
+            setcolor(4);
+            cout<<"Not enough "<< ingredient <<"in inventory";
+        }
+    }
+            setcolor(9);
+            typetext("The inventory is successfully updated ! ");
+
+        cout<<"if you want to check press 0, otherwise interested press any key";
+        bool check;
+        cin>>check;
+        if(!check)
+        {
+            InventoryManagement();
+        }
+        else
+        {
+            system("cls");
+            typetext(" end ");
+        }
+    }
+
+
 
 
 int main()
 {
     typetext("===== Welcome to DOT Hotel Management System! ===== \n \n Please choose an option:\n");
 
-    //setcolor(); //كل واحدة تختار اللون اللي هتستمر بيه في السكشن بتاعها!
+    setcolor(1); //كل واحدة تختار اللون اللي هتستمر بيه في السكشن بتاعها!
     typetext("1. Room Management.\n");
-    //setcolor();
+    setcolor(9);
 
     typetext("2. Food and Beverage Management.\n");
     setcolor(5);
@@ -95,32 +215,30 @@ int main()
 
     typetext("4. Bill and Payment.\n");
     int choose;
-    while (true)
+    cout << "Please enter a number between 1 and 4: ";
+    cin >> choose;
+    while (!isValidNumber(choose, 1, 4))
     {
+        setcolor(4);
+        cout << "Invalid input! Please enter a valid number between 1 and 4: ";
         setcolor(7);
-        cout << "Please enter a number between 1 and 4:\n ";
-        if (isValidNumber(choose) && choose >= 1 && choose <= 4)
-        {
-            break;
-        }
-        else
-        {  setcolor(4);
-            cout << "Invalid input! Please enter a valid number.\n";
-        }
+        cin >> choose;
     }
+
     system("cls");
     switch(choose)
     {
     case(1):
     {
-        //setcolor();
+        setcolor(1);
         typetext ("\t\t ===== Room Management ===== \n\n");
         break;
     }
     case(2):
     {
-        //setcolor();
-        typetext ("\t\t===== Food and Beverage Management ===== \n\n");
+        setcolor(9);
+        typetext ("\t\t===== Food and Beverage Management ===== /n");
+        foodandbevareg();
         break;
     }
     case(3):
